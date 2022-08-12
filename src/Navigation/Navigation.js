5ecd21094335e3395912290351c2a3c8d662946b
John Ore <jore@ofbf.org>
2022-08-12 12:19:01 -0400

    - Starting to build out navigation

---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,35 @@
+// Libraries
+
+import styled from 'styled-components';
+
+// Dependencies 
+
+// Styled Components
+const Wrapper = styled.nav`
+    display: flex;
+    flex-basis: 100%;
+`;
+
+const NavLinkLogoWrapper = styled.div`
+    display: flex;
+    flex-basis: 60%;
+`;
+
+const NavLinkWrapper = styled.div`
+    display: flex;
+    flex-basis: 40%;
+`;
+
+const Navigation = () => (
+    <Wrapper>
+        <NavLinkLogoWrapper>
+            <NavLink type="logo">Logo</NavLink>
+        </NavLinkLogoWrapper>
+        <NavLinkWrapper>
+            <NavLink type="link">Home</NavLink>
+            <NavLink type="link">Portfolio</NavLink>
+            <NavLink type="link">About</NavLink>
+            <NavLink type="link">Contact</NavLink>
+        </NavLinkWrapper>
+    </Wrapper>
+)
