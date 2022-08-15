e3e1c928aae50c0faaed2cb5967fdb1e6ea78832
John Ore <jore@ofbf.org>
2022-08-15 12:52:42 -0400

    - Adding hero component to app - Setting flex layout and default padding

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import styled from 'styled-components';
 // Dependencies 
 
 import Navigation from './Navigation/Navigation';
+import Hero from './Hero/Hero';
 
 // Styled Components
 
 const Main = styled.main`
   background: #000;
   margin: 5rem 6.875rem 4.75rem;
+  display: flex;
+  position: relative;
+  flex-direction: column;
 `;
 
 const App = () => (
     <Main>
       <Navigation />
+      <Hero />
     </Main>
 );
 
